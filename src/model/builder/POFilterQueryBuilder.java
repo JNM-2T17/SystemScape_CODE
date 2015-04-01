bb6a500c78026f0d6acb9fde3d2e7fb4126acbbc
Caisorda <juan_salceda@dlsu.edu.ph>
2015-04-02 00:00:57 +0800

    Purchase Order Query Builder

--- src/model/builder/POFilterQueryBuilder.java
+++ src/model/builder/POFilterQueryBuilder.java
@@ -31,59 +31,57 @@ public class POFilterQueryBuilder implements FilterQueryBuilder{
 
 	public void addColumn(String column) {
 		if(select.length()==0)
-			select= "SELECT \""+column+"\", ";
+			select= "SELECT "+column+", ";
 		else
-			select = select + "\"" + column + "\",";
+			select = select + "" + column + ", ";
 	}
 
 	public void addTable(String table) {
             if(from.length()==0)
-                from= "FROM \""+table+"\", ";
+                from= "FROM "+table+", ";
             else
-                from = from + "\"" + table + "\",";
+                from = from + "" + table + ", ";
 	}
 
         public void addCondition(Iterator conditions) {
             String temp;
             temp= (String) conditions.next();
             if(!temp.equals(""))
-                where = where + "po.type = \"" + temp +"\"&&";
+                where = where + "po.type = " + temp +" && ";
             temp= (String) conditions.next();
             if(!temp.equals(""))
-                where = where + "po.supplier = \"" + temp +"\"&&";
+                where = where + "po.supplier = " + temp +" && ";
             temp= (String) conditions.next();
             if(!temp.equals(""))
-                where = where + "po.date = \"" + temp +"\"&&";
+                where = where + "po.date = " + temp +" && ";
             temp= (String) conditions.next();
             if(!temp.equals(""))
-                where = where + "pi.idname = \"" + temp +"\"&&";
+                where = where + "pi.no = " + temp +" && ";
             temp= (String) conditions.next();
             if(!temp.equals(""))
-                where = where + "pi.no = \"" + temp +"\"&&";
-            temp= (String) conditions.next();
-            if(!temp.equals(""))
-                addGroupingCondition("Sum="+temp);
+                addGroupingCondition("Sum"+temp);
+            where = where + "po.no = pi.no && po.type = pi.type && pi.itemname = id.name && ";
 	}
 
 	public void addGrouping(String group) {
 		if(groupBy.length()==0)
-			groupBy = "GROUP BY \""+group+"\", ";
+			groupBy = "GROUP BY "+group+", ";
 		else
-			groupBy = groupBy + "\""+ group +"\",";
+			groupBy = groupBy + ""+ group +", ";
 	}
 
 	public void addGroupingCondition(String groupCondition) {
             if(having.length()==0)
-                having = "HAVING \""+ groupCondition +"\", ";
+                having = "HAVING "+ groupCondition +", ";
             else
-                having = having +" \""+groupCondition+"\", ";
+                having = having +" "+groupCondition+", ";
 	}
 
 	public void addOrderCriteria(String orderCriteria) {
             if(orderBy.length()==0)
-                orderBy= "ORDER BY \""+ orderCriteria+"\", ";
+                orderBy= "ORDER BY "+ orderCriteria+", ";
             else		
-                orderBy = orderBy + "\""+ orderCriteria +"\",";
+                orderBy = orderBy + ""+ orderCriteria +", ";
 	}
 
 	public String getQuery(Iterator conditions) {
@@ -95,8 +93,9 @@ public class POFilterQueryBuilder implements FilterQueryBuilder{
             addTable("purchaseorder po");
             addTable("poitem pi");
             addTable("itemdata id");
+            addGrouping("po.no");
+            addGrouping("po.type");
             addCondition(conditions);
-	
-            return select.substring(0,from.length()-1) + " " +from.substring(0,from.length()-1) + " " + where.substring(0,where.length()-2)+" "+groupBy.substring(0,groupBy.length()-1)+" "+having.substring(0,having.length()-1);
+            return select.substring(0,(select.length()-2)) + " " +from.substring(0,from.length()-2) + " " + where.substring(0,where.length()-3)+" "+groupBy.substring(0,groupBy.length()-2)+" "+having.substring(0,having.length()-2);
 	}
 }
