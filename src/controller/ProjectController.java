7851ea960223f7a175ede396c3de90e2492fce59
Bryan Coquilla <coquilla0906@gmail.com>
2015-04-15 01:58:38 +0800

    Revert "Project Controller Fix"
    This reverts commit 81971d2d0ce1dc80d6cba173bf32cb91688d3aba.

--- src/controller/ProjectController.java
+++ src/controller/ProjectController.java
@@ -37,13 +37,9 @@ public class ProjectController implements Subject {
         project = new Project();
     }
     
-    public Iterator search(String searchStr){
+     public Iterator search(String searchStr){
         return dao.search("project", searchStr);
     }
-     
-     public Iterator filter(Iterator conds){
-         return dao.filter("project", conds);
-     }
     
 
     public Project getProject() {
