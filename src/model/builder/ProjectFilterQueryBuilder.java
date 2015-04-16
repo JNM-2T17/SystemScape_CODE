a9d0147644c0343a1d1ccd1673f2278d8a84ba65
XGBXGB <kewpee10@hotmail.com>
2015-04-17 04:16:01 +0800

    Remade Filter Project

--- src/model/builder/ProjectFilterQueryBuilder.java
+++ src/model/builder/ProjectFilterQueryBuilder.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model.builder;
 
 import java.util.Date;
@@ -13,89 +12,99 @@ import java.util.Iterator;
  *
  * @author Christian Gabriel
  */
-public class ProjectFilterQueryBuilder implements FilterQueryBuilder{
+public class ProjectFilterQueryBuilder implements FilterQueryBuilder {
+
     private String select;
-	private String from;
-	private String where;
-	private String groupBy;
-	private String having;
-	private String orderBy;
+    private String from;
+    private String where;
+    private String groupBy;
+    private String having;
+    private String orderBy;
 
-	public ProjectFilterQueryBuilder() {
-		select="";
-		from=""; 
-		where="WHERE ";
-		groupBy="";
-		having="";
-		orderBy="";
-	}
+    public ProjectFilterQueryBuilder() {
+        select = "";
+        from = "";
+        where = "WHERE ";
+        groupBy = "";
+        having = "";
+        orderBy = "";
+    }
 
-	public void addColumn(String column) {
-		if(select.length()==0)
-			select= "SELECT "+column+", ";
-		else
-			select = select + "" + column + ", ";
-	}
+    public void addColumn(String column) {
+        if (select.length() == 0) {
+            select = "SELECT " + column + ", ";
+        } else {
+            select = select + "" + column + ", ";
+        }
+    }
 
-	public void addTable(String table) {
-            if(from.length()==0)
-                from= "FROM "+table+", ";
-            else
-                from = from + "" + table + ", ";
-	}
+    public void addTable(String table) {
+        if (from.length() == 0) {
+            from = "FROM " + table + ", ";
+        } else {
+            from = from + "" + table + ", ";
+        }
+    }
 
     public void addCondition(Iterator conditions) {
-    	
-    	if(conditions!=null){
-        
+
+        if (conditions != null) {
+
             String temp;
-            temp= (String) conditions.next();
-            if(!temp.equals(""))
-                where = where + "p.name LIKE \"%" + temp +"%\" OR ";
-            temp= ((Date) conditions.next()).toString();
-            if(!temp.equals(""))
-                where = where + "p.startDate LIKE \"%" + temp +"%\" OR ";
-            temp= ((Date) conditions.next()).toString();
-            if(!temp.equals(""))
-                where = where + "p.endDate LIKE \"%" + temp +"%\" OR ";
-            
-            where = where.substring(0, where.length() - 2);
-    	}
-    	else
-    		where = "";
-    	
-	}
-
-	public void addGrouping(String group) {
-		if(groupBy.length()==0)
-			groupBy = "GROUP BY "+group+", ";
-		else
-			groupBy = groupBy + ""+ group +", ";
-	}
-
-	public void addGroupingCondition(String groupCondition) {
-            if(having.length()==0)
-                having = "HAVING "+ groupCondition +", ";
-            else
-                having = having +" "+groupCondition+", ";
-	}
-
-	public void addOrderCriteria(String orderCriteria) {
-            if(orderBy.length()==0)
-                orderBy= "ORDER BY "+ orderCriteria+", ";
-            else		
-                orderBy = orderBy + ""+ orderCriteria +", ";
-	}
-
-	public String getQuery(Iterator conditions) {
-            addColumn("p.name");
-            addColumn("p.startDate");
-            addColumn("p.endDate");
-            addTable("project p");
-            addCondition(conditions);
-            if (groupBy.length() > 0) {
-                groupBy = groupBy.substring(0, groupBy.length() - 1);
+            temp = (String) conditions.next();
+            if (!temp.equals("")) {
+                where = where + "p.name LIKE \"%" + temp + "%\" && ";
             }
-            return select.substring(0,(select.length()-2)) + " " +from.substring(0,from.length()-2) + " " + where.substring(0,where.length()-3);
-	}
+            temp = (String) conditions.next();
+            if (!temp.equals("")) {
+                where = where + "p.startDate LIKE \"%" + temp + "%\" && ";
+            }
+            temp = (String) conditions.next();
+            if (!temp.equals("")) {
+                where = where + "p.endDate LIKE \"%" + temp + "%\" && ";
+            }
+
+            where = where.substring(0, where.length() - 3);
+        } else {
+            where = "";
+
+        }
+    }
+
+    public void addGrouping(String group) {
+        if (groupBy.length() == 0) {
+            groupBy = "GROUP BY " + group + ", ";
+        } else {
+            groupBy = groupBy + "" + group + ", ";
+        }
+    }
+
+    public void addGroupingCondition(String groupCondition) {
+        if (having.length() == 0) {
+            having = "HAVING " + groupCondition + ", ";
+        } else {
+            having = having + " " + groupCondition + ", ";
+        }
+    }
+
+    public void addOrderCriteria(String orderCriteria) {
+        if (orderBy.length() == 0) {
+            orderBy = "ORDER BY " + orderCriteria + ", ";
+        } else {
+            orderBy = orderBy + "" + orderCriteria + ", ";
+        }
+    }
+
+    public String getQuery(Iterator conditions) {
+        addColumn("p.name");
+        addColumn("p.startDate");
+        addColumn("p.endDate");
+        addTable("project p");
+        addCondition(conditions);
+        if (groupBy.length() > 0) {
+            groupBy = groupBy.substring(0, groupBy.length() - 1);
+        }
+        System.out.println("where: "+where);
+        return select.substring(0, (select.length() - 2)) + " " + from.substring(0, from.length() - 2) + " " + where;
+    }
 }
--- src/view/projects/FilterProject.java
+++ src/view/projects/FilterProject.java
@@ -30,196 +30,206 @@ import controller.ProjectController;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 
 public class FilterProject extends PopUp implements ActionListener {
-	private JComboBox cmbName;
-	private JDateChooser dateStart;
-	private JDateChooser dateEnd;
-	private JButton btnFilter, btnRemoveFilter;
-	private boolean closed = true;
-	private ProjectController projectController;
-	private ArrayList list;
-	
-	public FilterProject(JFrame parent) {
-		super(parent);
-		projectController = ProjectController.getInstance();
-		list=new ArrayList();
-		JPanel panMain = new JPanel();
-		panMain.setLayout(new BorderLayout(0, 0));
 
-		JPanel panHeader = new JPanel();
-		panHeader.setBackground(Color.WHITE);
-		panMain.add(panHeader, BorderLayout.NORTH);
+    private JComboBox cmbName;
+    private JDateChooser dateStart;
+    private JDateChooser dateEnd;
+    private JButton btnFilter, btnRemoveFilter;
+    private boolean closed = true;
+    private ProjectController projectController;
+    private ArrayList list;
 
-		JLabel lblFilterProj = new JLabel("Filter Projects");
-		lblFilterProj.setFont(new Font("Arial", Font.PLAIN, 16));
-		panHeader.add(lblFilterProj);
+    public FilterProject(JFrame parent) {
+        super(parent);
+        projectController = ProjectController.getInstance();
+        list = new ArrayList();
+        JPanel panMain = new JPanel();
+        panMain.setLayout(new BorderLayout(0, 0));
 
-		JPanel panCenter = new JPanel();
-		panCenter.setBackground(Color.WHITE);
-		panMain.add(panCenter, BorderLayout.CENTER);
-		SpringLayout sl_panCenter = new SpringLayout();
-		panCenter.setLayout(sl_panCenter);
+        JPanel panHeader = new JPanel();
+        panHeader.setBackground(Color.WHITE);
+        panMain.add(panHeader, BorderLayout.NORTH);
 
-		JLabel lblProjectTitle = new JLabel("Project Title:");
-		sl_panCenter.putConstraint(SpringLayout.NORTH, lblProjectTitle, 38,
-				SpringLayout.NORTH, panCenter);
-		sl_panCenter.putConstraint(SpringLayout.WEST, lblProjectTitle, 55,
-				SpringLayout.WEST, panCenter);
-		panCenter.add(lblProjectTitle);
+        JLabel lblFilterProj = new JLabel("Filter Projects");
+        lblFilterProj.setFont(new Font("Arial", Font.PLAIN, 16));
+        panHeader.add(lblFilterProj);
 
-		JLabel lblStartDate = new JLabel("Start Date:");
-		sl_panCenter.putConstraint(SpringLayout.NORTH, lblStartDate, 16,
-				SpringLayout.SOUTH, lblProjectTitle);
-		sl_panCenter.putConstraint(SpringLayout.WEST, lblStartDate, 0,
-				SpringLayout.WEST, lblProjectTitle);
-		panCenter.add(lblStartDate);
+        JPanel panCenter = new JPanel();
+        panCenter.setBackground(Color.WHITE);
+        panMain.add(panCenter, BorderLayout.CENTER);
+        SpringLayout sl_panCenter = new SpringLayout();
+        panCenter.setLayout(sl_panCenter);
 
-		JLabel lblEndDate = new JLabel("End Date:");
-		sl_panCenter.putConstraint(SpringLayout.NORTH, lblEndDate, 16,
-				SpringLayout.SOUTH, lblStartDate);
-		sl_panCenter.putConstraint(SpringLayout.WEST, lblEndDate, 0,
-				SpringLayout.WEST, lblProjectTitle);
-		panCenter.add(lblEndDate);
+        JLabel lblProjectTitle = new JLabel("Project Title:");
+        sl_panCenter.putConstraint(SpringLayout.NORTH, lblProjectTitle, 38,
+                SpringLayout.NORTH, panCenter);
+        sl_panCenter.putConstraint(SpringLayout.WEST, lblProjectTitle, 55,
+                SpringLayout.WEST, panCenter);
+        panCenter.add(lblProjectTitle);
 
-		cmbName = new JComboBox();
-		AutoCompleteDecorator.decorate(cmbName);
-		sl_panCenter.putConstraint(SpringLayout.NORTH, cmbName, 32,
-				SpringLayout.NORTH, panCenter);
-		sl_panCenter.putConstraint(SpringLayout.WEST, cmbName, 15,
-				SpringLayout.EAST, lblProjectTitle);
-		sl_panCenter.putConstraint(SpringLayout.EAST, cmbName, 285,
-				SpringLayout.EAST, lblProjectTitle);
-		panCenter.add(cmbName);
+        JLabel lblStartDate = new JLabel("Start Date:");
+        sl_panCenter.putConstraint(SpringLayout.NORTH, lblStartDate, 16,
+                SpringLayout.SOUTH, lblProjectTitle);
+        sl_panCenter.putConstraint(SpringLayout.WEST, lblStartDate, 0,
+                SpringLayout.WEST, lblProjectTitle);
+        panCenter.add(lblStartDate);
 
-		dateStart = new JDateChooser();
-		dateStart.setDateFormatString("yyyy-MM-dd");
-		sl_panCenter.putConstraint(SpringLayout.WEST, dateStart, 0,
-				SpringLayout.WEST, cmbName);
-		sl_panCenter.putConstraint(SpringLayout.SOUTH, dateStart, 0,
-				SpringLayout.SOUTH, lblStartDate);
-		sl_panCenter.putConstraint(SpringLayout.EAST, dateStart, 0,
-				SpringLayout.EAST, cmbName);
-		panCenter.add(dateStart);
+        JLabel lblEndDate = new JLabel("End Date:");
+        sl_panCenter.putConstraint(SpringLayout.NORTH, lblEndDate, 16,
+                SpringLayout.SOUTH, lblStartDate);
+        sl_panCenter.putConstraint(SpringLayout.WEST, lblEndDate, 0,
+                SpringLayout.WEST, lblProjectTitle);
+        panCenter.add(lblEndDate);
 
-		dateEnd = new JDateChooser();
-		dateEnd.setDateFormatString("yyyy-MM-dd");
-		sl_panCenter.putConstraint(SpringLayout.WEST, dateEnd, 0,
-				SpringLayout.WEST, cmbName);
-		sl_panCenter.putConstraint(SpringLayout.SOUTH, dateEnd, 0,
-				SpringLayout.SOUTH, lblEndDate);
-		sl_panCenter.putConstraint(SpringLayout.EAST, dateEnd, 0,
-				SpringLayout.EAST, cmbName);
-		panCenter.add(dateEnd);
+        cmbName = new JComboBox();
+        AutoCompleteDecorator.decorate(cmbName);
+        sl_panCenter.putConstraint(SpringLayout.NORTH, cmbName, 32,
+                SpringLayout.NORTH, panCenter);
+        sl_panCenter.putConstraint(SpringLayout.WEST, cmbName, 15,
+                SpringLayout.EAST, lblProjectTitle);
+        sl_panCenter.putConstraint(SpringLayout.EAST, cmbName, 285,
+                SpringLayout.EAST, lblProjectTitle);
+        panCenter.add(cmbName);
 
-		JPanel panFooter = new JPanel();
-		panFooter.setBackground(Color.white);
-		panMain.add(panFooter, BorderLayout.SOUTH);
+        dateStart = new JDateChooser();
+        dateStart.setDateFormatString("yyyy-MM-dd");
+        sl_panCenter.putConstraint(SpringLayout.WEST, dateStart, 0,
+                SpringLayout.WEST, cmbName);
+        sl_panCenter.putConstraint(SpringLayout.SOUTH, dateStart, 0,
+                SpringLayout.SOUTH, lblStartDate);
+        sl_panCenter.putConstraint(SpringLayout.EAST, dateStart, 0,
+                SpringLayout.EAST, cmbName);
+        panCenter.add(dateStart);
 
-		btnFilter = new JButton("Filter");
-		btnFilter.setFont(new Font("Arial", Font.PLAIN, 18));
-		panFooter.add(btnFilter);
-		btnFilter.setForeground(Color.white);
-		btnFilter.setBackground(new Color(32, 130, 213));
-		panFooter.add(btnFilter);
+        dateEnd = new JDateChooser();
+        dateEnd.setDateFormatString("yyyy-MM-dd");
+        sl_panCenter.putConstraint(SpringLayout.WEST, dateEnd, 0,
+                SpringLayout.WEST, cmbName);
+        sl_panCenter.putConstraint(SpringLayout.SOUTH, dateEnd, 0,
+                SpringLayout.SOUTH, lblEndDate);
+        sl_panCenter.putConstraint(SpringLayout.EAST, dateEnd, 0,
+                SpringLayout.EAST, cmbName);
+        panCenter.add(dateEnd);
 
+        JPanel panFooter = new JPanel();
+        panFooter.setBackground(Color.white);
+        panMain.add(panFooter, BorderLayout.SOUTH);
 
-		btnRemoveFilter = new JButton("Remove Filter");
-		btnRemoveFilter.setForeground(new Color(255, 255, 255));
-		btnRemoveFilter.setFont(new Font("Arial", Font.PLAIN, 18));
-		btnRemoveFilter.setBackground(new Color(32, 130, 213));
-		btnRemoveFilter.addActionListener(this);
-		panFooter.add(btnRemoveFilter);
+        btnFilter = new JButton("Filter");
+        btnFilter.setFont(new Font("Arial", Font.PLAIN, 18));
+        panFooter.add(btnFilter);
+        btnFilter.setForeground(Color.white);
+        btnFilter.setBackground(new Color(32, 130, 213));
+        panFooter.add(btnFilter);
 
-		
-		panMain.setPreferredSize(new Dimension(480, 250));
-		panMain.setSize(new Dimension(480, 250));
-		setContent(panMain);
-		getClose().addActionListener(this);
-		btnFilter.addActionListener(this);
-		populateProjectNames();
-		setVisible(true);
-		this.repaint();
-		this.revalidate();
-		
-	
-	}
+        btnRemoveFilter = new JButton("Remove Filter");
+        btnRemoveFilter.setForeground(new Color(255, 255, 255));
+        btnRemoveFilter.setFont(new Font("Arial", Font.PLAIN, 18));
+        btnRemoveFilter.setBackground(new Color(32, 130, 213));
+        btnRemoveFilter.addActionListener(this);
+        panFooter.add(btnRemoveFilter);
 
-	public Iterator getValues() {
-		
-		if(list!=null){
-	        list.add((String)cmbName.getSelectedItem());
-			list.add(new java.sql.Date(dateStart.getDate().getTime()));
-			list.add(new java.sql.Date(dateEnd.getDate().getTime()));
-	
-			return list.iterator();
-		}
-		return null;
-	}
+        panMain.setPreferredSize(new Dimension(480, 250));
+        panMain.setSize(new Dimension(480, 250));
+        setContent(panMain);
+        getClose().addActionListener(this);
+        btnFilter.addActionListener(this);
+        populateProjectNames();
+        setVisible(true);
+        this.repaint();
+        this.revalidate();
 
-	public boolean isClosed() {
-		return closed;
-	}
+    }
 
-	public boolean checkFields()
-	{
-		boolean isEmpty = false;
-		if(cmbName.getSelectedIndex() == 0 && dateStart.getDate() == null && dateEnd.getDate() == null)
-		{
-			isEmpty = true;
-		}
-		return isEmpty;
-	} 
+    public Iterator getValues() {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getSource() == getClose()) {
-			this.dispose();
-		} 
-		/****
-		 * DEV Insert Code statements here to filter the list of suppliers 
-		 * if at least one of the fields is not empty/ meaning may laman kahit isa sa fields
-		 * kung mern then proceed to filtering the list
-		 *****/
-		else if (checkFields() == false && e.getSource() == btnFilter) {
-			closed = false;
-			this.dispose();
-		}
-		else if (checkFields() == true && e.getSource() == btnFilter ){
-			/****
-			 * DEV Insert Code statements here to set the 
-			 * list to the original if not one of the fields is filled up/
-			 * meaning if All fields are empty wag mag filter but insetad revert it back to the original
-			 * list of suppliers
-			 *****/
-			list = null;
+        if (list != null) {
+            list.add((String) cmbName.getSelectedItem());
+
+            if (dateStart.getDate() == null) {
+                list.add("");
+            } else {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                dateFormat.format(dateStart.getDate());
+                list.add("" + dateFormat.format(dateStart.getDate()));
+            }
+            if (dateEnd.getDate() == null) {
+                list.add("");
+            } else {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                dateFormat.format(dateEnd.getDate());
+                list.add("" + dateFormat.format(dateEnd.getDate()));
+            }
+            
+
+            return list.iterator();
+        }
+        return null;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public boolean checkFields() {
+        boolean isEmpty = false;
+        if (cmbName.getSelectedIndex() == 0 && dateStart.getDate() == null && dateEnd.getDate() == null) {
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource() == getClose()) {
+            this.dispose();
+        } /**
+         * **
+         * DEV Insert Code statements here to filter the list of suppliers if at
+         * least one of the fields is not empty/ meaning may laman kahit isa sa
+         * fields kung mern then proceed to filtering the list ***
+         */
+        else if (checkFields() == false && e.getSource() == btnFilter) {
             closed = false;
-			this.dispose();
-		}
-		else if (e.getSource() == btnRemoveFilter){
-			/***
-			 * DEV insert code statements here to remove the filter and set the view table to the original
-			 * meaning yung walang filter...
-			*****/
-			list = null;
+            this.dispose();
+        } else if (checkFields() == true && e.getSource() == btnFilter) {
+            /**
+             * **
+             * DEV Insert Code statements here to set the list to the original
+             * if not one of the fields is filled up/ meaning if All fields are
+             * empty wag mag filter but insetad revert it back to the original
+             * list of suppliers ***
+             */
+            list = null;
             closed = false;
-			this.dispose();
-		}
-	}
-	
-	public void populateProjectNames(){
-		Iterator<Project> iterator = projectController.getAll();
-		ArrayList<String> projectNames = new ArrayList();
-		projectNames.add("");
-		while(iterator.hasNext()){
-			projectNames.add(iterator.next().getName());
-		}
-		cmbName.setModel(new DefaultComboBoxModel(projectNames.toArray()));
-	}
-	
-	
+            this.dispose();
+        } else if (e.getSource() == btnRemoveFilter) {
+            /**
+             * *
+             * DEV insert code statements here to remove the filter and set the
+             * view table to the original meaning yung walang filter... ***
+             */
+            list = null;
+            closed = false;
+            this.dispose();
+        }
+    }
+
+    public void populateProjectNames() {
+        Iterator<Project> iterator = projectController.getAll();
+        ArrayList<String> projectNames = new ArrayList();
+        projectNames.add("");
+        while (iterator.hasNext()) {
+            projectNames.add(iterator.next().getName());
+        }
+        cmbName.setModel(new DefaultComboBoxModel(projectNames.toArray()));
+    }
+
 }
