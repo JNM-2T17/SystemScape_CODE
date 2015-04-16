f921d57fc8d4d89b37562006d3458da1f8a9c967
XGBXGB <kewpee10@hotmail.com>
2015-04-17 03:26:48 +0800

    Fixed Employee Filter

--- src/model/builder/EmployeeFilterQueryBuilder.java
+++ src/model/builder/EmployeeFilterQueryBuilder.java
@@ -4,99 +4,110 @@ import java.util.Date;
 import java.util.Iterator;
 
 public class EmployeeFilterQueryBuilder implements FilterQueryBuilder {
-	
-	private String select;
-	private String from;
-	private String where;
-	private String groupBy;
-	private String having;
-	private String orderBy;
 
-	public EmployeeFilterQueryBuilder() {
-		select="";
-		from=""; 
-		where="WHERE ";
-		groupBy="";
-		having="";
-		orderBy="";
-	}
+    private String select;
+    private String from;
+    private String where;
+    private String groupBy;
+    private String having;
+    private String orderBy;
 
-	public void addColumn(String column) {
-		if(select.length()==0)
-			select= "SELECT "+column+", ";
-		else
-			select = select + "" + column + ", ";
-	}
+    public EmployeeFilterQueryBuilder() {
+        select = "";
+        from = "";
+        where = "WHERE ";
+        groupBy = "";
+        having = "";
+        orderBy = "";
+    }
 
-	public void addTable(String table) {
-            if(from.length()==0)
-                from= "FROM "+table+", ";
-            else
-                from = from + "" + table + ", ";
-	}
+    public void addColumn(String column) {
+        if (select.length() == 0) {
+            select = "SELECT " + column + ", ";
+        } else {
+            select = select + "" + column + ", ";
+        }
+    }
 
-        public void addCondition(Iterator conditions) {
+    public void addTable(String table) {
+        if (from.length() == 0) {
+            from = "FROM " + table + ", ";
+        } else {
+            from = from + "" + table + ", ";
+        }
+    }
+
+    public void addCondition(Iterator conditions) {
+        if (conditions != null) {
             String temp;
-            temp= (String) conditions.next();
-            if(!temp.equals(""))
-                where = where + "e.name LIKE \"%" + temp +"%\" OR ";
-            temp= conditions.next().toString();
-            if(!temp.equals(""))
-                where = where + "e.status LIKE \"%" + temp +"%\" OR ";
-            temp= conditions.next().toString();
-            if(!temp.equals(""))
-                where = where + "p.name LIKE \"%" + temp +"%\" OR ";
-           
-            where = where.substring(0, where.length()-3);
-            
-            System.out.println("WHERE na you"+ where);
-	}
+            temp = (String) conditions.next();
+            if (!temp.equals("")) {
+                where = where + "e.name LIKE \"%" + temp + "%\" OR ";
+            }
+            temp = conditions.next().toString();
+            if (!temp.equals("")) {
+                where = where + "e.status LIKE \"%" + temp + "%\" OR ";
+            }
+            temp = conditions.next().toString();
+            if (!temp.equals("")) {
+                where = where + "p.name LIKE \"%" + temp + "%\" OR ";
+            }
 
-	public void addGrouping(String group) {
-		if(groupBy.length()==0)
-			groupBy = "GROUP BY "+group+", ";
-		else
-			groupBy = groupBy + ""+ group +", ";
-	}
+            where = where.substring(0, where.length() - 3);
 
-	public void addGroupingCondition(String groupCondition) {
-            if(having.length()==0)
-                having = "HAVING "+ groupCondition +", ";
-            else
-                having = having +" "+groupCondition+", ";
-	}
-
-	public void addOrderCriteria(String orderCriteria) {
-            if(orderBy.length()==0)
-                orderBy= "ORDER BY "+ orderCriteria+", ";
-            else		
-                orderBy = orderBy + ""+ orderCriteria +", ";
-	}
-        
-        public void addLeftJoin(String table){
-            from = from +"LEFT JOIN "+ table+" ";
+        } else {
+            where = "";
         }
-        
-        public void addOn(String condition){
-            from = from+"ON "+condition+" ";
+    }
+
+    public void addGrouping(String group) {
+        if (groupBy.length() == 0) {
+            groupBy = "GROUP BY " + group + ", ";
+        } else {
+            groupBy = groupBy + "" + group + ", ";
         }
-        
-	public String getQuery(Iterator conditions) {
-            addColumn("e.ID");
-            addColumn("e.name");
-            addColumn("e.status");
-            addColumn("p.name");
-            addColumn("p.startDate");
-            addColumn("p.endDate");
-            addTable("employee e");
-            from = from.substring(0,from.length()-2) + " ";
-            addLeftJoin("projectassignment pa");
-            addOn("pa.employeeID = e.ID");
-            addLeftJoin("project p");
-            addOn("pa.project = p.name");
-            addCondition(conditions);
-            System.out.println(select.substring(0,(select.length()-2)) + " " +from.substring(0,from.length()-1) + " " + where);
-            return select.substring(0,(select.length()-2)) + " " +from.substring(0,from.length()-1) + " " + where;
-	}
+    }
+
+    public void addGroupingCondition(String groupCondition) {
+        if (having.length() == 0) {
+            having = "HAVING " + groupCondition + ", ";
+        } else {
+            having = having + " " + groupCondition + ", ";
+        }
+    }
+
+    public void addOrderCriteria(String orderCriteria) {
+        if (orderBy.length() == 0) {
+            orderBy = "ORDER BY " + orderCriteria + ", ";
+        } else {
+            orderBy = orderBy + "" + orderCriteria + ", ";
+        }
+    }
+
+    public void addLeftJoin(String table) {
+        from = from + "LEFT JOIN " + table + " ";
+    }
+
+    public void addOn(String condition) {
+        from = from + "ON " + condition + " ";
+    }
+
+    public String getQuery(Iterator conditions) {
+        addColumn("e.ID");
+        addColumn("e.name");
+        addColumn("e.status");
+        addColumn("p.name");
+        addColumn("p.startDate");
+        addColumn("p.endDate");
+        addTable("employee e");
+        from = from.substring(0, from.length() - 2) + " ";
+        addLeftJoin("projectassignment pa");
+        addOn("pa.employeeID = e.ID");
+        addLeftJoin("project p");
+        addOn("pa.project = p.name");
+        addCondition(conditions);
+        System.out.println(select.substring(0, (select.length() - 2)) + " " + from.substring(0, from.length() - 1) + " " + where);
+        return select.substring(0, (select.length() - 2)) + " " + from.substring(0, from.length() - 1) + " " + where;
+    }
 
 }
--- src/view/employee/FilterEmployee.java
+++ src/view/employee/FilterEmployee.java
@@ -33,207 +33,211 @@ import view.PopUp;
 
 public class FilterEmployee extends PopUp implements ActionListener {
 
-	private JButton btnFilter, btnRemoveFilter;
-	private JComboBox cmbName;
-	private JComboBox cmbStatus, cmbProject;
+    private JButton btnFilter, btnRemoveFilter;
+    private JComboBox cmbName;
+    private JComboBox cmbStatus, cmbProject;
     private boolean closed = true;
     private EmployeeController employeeController;
     private ProjectController projectController;
+    private ArrayList list;
 
-	public FilterEmployee(JFrame parent) {
-		super(parent);
-		employeeController = EmployeeController.getInstance();
-		projectController = ProjectController.getInstance();
-		JPanel panMain = new JPanel();
-		panMain.setBackground(Color.WHITE);
-		panMain.setPreferredSize(new Dimension(450, 250));
-		panMain.setSize(new Dimension(450, 250));
-		getContentPane().add(panMain);
-		panMain.setLayout(new BorderLayout(0, 0));
+    public FilterEmployee(JFrame parent) {
+        super(parent);
+        list = new ArrayList();
+        employeeController = EmployeeController.getInstance();
+        projectController = ProjectController.getInstance();
+        JPanel panMain = new JPanel();
+        panMain.setBackground(Color.WHITE);
+        panMain.setPreferredSize(new Dimension(450, 250));
+        panMain.setSize(new Dimension(450, 250));
+        getContentPane().add(panMain);
+        panMain.setLayout(new BorderLayout(0, 0));
 
-		JPanel panHeader = new JPanel();
-		panHeader.setBackground(Color.WHITE);
-		panMain.add(panHeader, BorderLayout.NORTH);
+        JPanel panHeader = new JPanel();
+        panHeader.setBackground(Color.WHITE);
+        panMain.add(panHeader, BorderLayout.NORTH);
 
-		JLabel lblFilterEmp = new JLabel("Filter Employees");
-		lblFilterEmp.setFont(new Font("Arial", Font.PLAIN, 16));
-		panHeader.add(lblFilterEmp);
-		
-		JPanel panFooter = new JPanel();
-		panFooter.setBackground(Color.WHITE);
-		panMain.add(panFooter, BorderLayout.SOUTH);
+        JLabel lblFilterEmp = new JLabel("Filter Employees");
+        lblFilterEmp.setFont(new Font("Arial", Font.PLAIN, 16));
+        panHeader.add(lblFilterEmp);
 
-		btnFilter = new JButton("Filter");
-		panFooter.add(btnFilter);
-		btnFilter.addActionListener(this);
-		btnFilter.setForeground(Color.white);
-		btnFilter.setBackground(new Color(32, 130, 213));
-		btnFilter.setFont(new Font("Arial", Font.PLAIN, 18));
-		panFooter.add(btnFilter);
+        JPanel panFooter = new JPanel();
+        panFooter.setBackground(Color.WHITE);
+        panMain.add(panFooter, BorderLayout.SOUTH);
 
-		btnRemoveFilter = new JButton("Remove Filter");
-		btnRemoveFilter.setForeground(new Color(255, 255, 255));
-		btnRemoveFilter.setFont(new Font("Arial", Font.PLAIN, 18));
-		btnRemoveFilter.setBackground(new Color(32, 130, 213));
-		btnRemoveFilter.addActionListener(this);
-		panFooter.add(btnRemoveFilter);
-		
-		JPanel panContent = new JPanel();
-		panContent.setBackground(Color.WHITE);
-		panMain.add(panContent, BorderLayout.CENTER);
-		SpringLayout sl_panContent = new SpringLayout();
-		panContent.setLayout(sl_panContent);
+        btnFilter = new JButton("Filter");
+        panFooter.add(btnFilter);
+        btnFilter.addActionListener(this);
+        btnFilter.setForeground(Color.white);
+        btnFilter.setBackground(new Color(32, 130, 213));
+        btnFilter.setFont(new Font("Arial", Font.PLAIN, 18));
+        panFooter.add(btnFilter);
 
-		JLabel lblName = new JLabel("Name:");
-		sl_panContent.putConstraint(SpringLayout.NORTH, lblName, 50,
-				SpringLayout.NORTH, panContent);
-		sl_panContent.putConstraint(SpringLayout.WEST, lblName, 50,
-				SpringLayout.WEST, panContent);
-		panContent.add(lblName);
+        btnRemoveFilter = new JButton("Remove Filter");
+        btnRemoveFilter.setForeground(new Color(255, 255, 255));
+        btnRemoveFilter.setFont(new Font("Arial", Font.PLAIN, 18));
+        btnRemoveFilter.setBackground(new Color(32, 130, 213));
+        btnRemoveFilter.addActionListener(this);
+        panFooter.add(btnRemoveFilter);
 
-		JLabel lblStatus = new JLabel("Status: ");
-		sl_panContent.putConstraint(SpringLayout.NORTH, lblStatus, 20,
-				SpringLayout.SOUTH, lblName);
-		sl_panContent.putConstraint(SpringLayout.WEST, lblStatus, 0,
-				SpringLayout.WEST, lblName);
-		panContent.add(lblStatus);
+        JPanel panContent = new JPanel();
+        panContent.setBackground(Color.WHITE);
+        panMain.add(panContent, BorderLayout.CENTER);
+        SpringLayout sl_panContent = new SpringLayout();
+        panContent.setLayout(sl_panContent);
 
-		cmbName = new JComboBox();
-		AutoCompleteDecorator.decorate(cmbName);
-		sl_panContent.putConstraint(SpringLayout.WEST, cmbName, 30,
-				SpringLayout.EAST, lblName);
-		sl_panContent.putConstraint(SpringLayout.SOUTH, cmbName, 0, SpringLayout.SOUTH, lblName);
-		sl_panContent.putConstraint(SpringLayout.EAST, cmbName, -50,
-				SpringLayout.EAST, panContent);
-		cmbName.setBackground(Color.white);
-		panContent.add(cmbName);
+        JLabel lblName = new JLabel("Name:");
+        sl_panContent.putConstraint(SpringLayout.NORTH, lblName, 50,
+                SpringLayout.NORTH, panContent);
+        sl_panContent.putConstraint(SpringLayout.WEST, lblName, 50,
+                SpringLayout.WEST, panContent);
+        panContent.add(lblName);
 
-		cmbStatus = new JComboBox();
-		cmbStatus.setEditable(true);
-		AutoCompleteDecorator.decorate(cmbStatus);
-		cmbStatus.setBackground(Color.WHITE);
-		sl_panContent.putConstraint(SpringLayout.WEST, cmbStatus, 0,
-				SpringLayout.WEST, cmbName);
-		sl_panContent.putConstraint(SpringLayout.SOUTH, cmbStatus, 0,
-				SpringLayout.SOUTH, lblStatus);
-		sl_panContent.putConstraint(SpringLayout.EAST, cmbStatus, 0,
-				SpringLayout.EAST, cmbName);
-		panContent.add(cmbStatus);
-		
-		JLabel lblProject = new JLabel("Project:");
-		sl_panContent.putConstraint(SpringLayout.NORTH, lblProject, 20, SpringLayout.SOUTH, lblStatus);
-		sl_panContent.putConstraint(SpringLayout.WEST, lblProject, 0, SpringLayout.WEST, lblName);
-		panContent.add(lblProject);
-		
-		cmbProject = new JComboBox();
-		cmbProject.setEditable(true);
-		AutoCompleteDecorator.decorate(cmbProject);
-		sl_panContent.putConstraint(SpringLayout.WEST, cmbProject, 0, SpringLayout.WEST, cmbName);
-		sl_panContent.putConstraint(SpringLayout.SOUTH, cmbProject, 0, SpringLayout.SOUTH, lblProject);
-		sl_panContent.putConstraint(SpringLayout.EAST, cmbProject, 0, SpringLayout.EAST, cmbName);
-		cmbProject.setBackground(Color.WHITE);
-		panContent.add(cmbProject);
+        JLabel lblStatus = new JLabel("Status: ");
+        sl_panContent.putConstraint(SpringLayout.NORTH, lblStatus, 20,
+                SpringLayout.SOUTH, lblName);
+        sl_panContent.putConstraint(SpringLayout.WEST, lblStatus, 0,
+                SpringLayout.WEST, lblName);
+        panContent.add(lblStatus);
 
-		getClose().addActionListener(this);
-                
-		setContent(panMain);
-		populateEmployeeNames();
-		populateEmployeeStatus();
-		populateEmployeeProjects();
-		this.setVisible(true);
-		this.repaint();
-		this.revalidate();
-	}
+        cmbName = new JComboBox();
+        AutoCompleteDecorator.decorate(cmbName);
+        sl_panContent.putConstraint(SpringLayout.WEST, cmbName, 30,
+                SpringLayout.EAST, lblName);
+        sl_panContent.putConstraint(SpringLayout.SOUTH, cmbName, 0, SpringLayout.SOUTH, lblName);
+        sl_panContent.putConstraint(SpringLayout.EAST, cmbName, -50,
+                SpringLayout.EAST, panContent);
+        cmbName.setBackground(Color.white);
+        panContent.add(cmbName);
 
-	public Iterator getValues() {
-		ArrayList list = new ArrayList();
-		
-                list.add((String)cmbName.getSelectedItem());
-		list.add((String)cmbStatus.getSelectedItem());
-		list.add((String)cmbProject.getSelectedItem());
-		return list.iterator();
-	}
+        cmbStatus = new JComboBox();
+        cmbStatus.setEditable(true);
+        AutoCompleteDecorator.decorate(cmbStatus);
+        cmbStatus.setBackground(Color.WHITE);
+        sl_panContent.putConstraint(SpringLayout.WEST, cmbStatus, 0,
+                SpringLayout.WEST, cmbName);
+        sl_panContent.putConstraint(SpringLayout.SOUTH, cmbStatus, 0,
+                SpringLayout.SOUTH, lblStatus);
+        sl_panContent.putConstraint(SpringLayout.EAST, cmbStatus, 0,
+                SpringLayout.EAST, cmbName);
+        panContent.add(cmbStatus);
 
-	public boolean isClosed() {
-		return closed;
-	}
+        JLabel lblProject = new JLabel("Project:");
+        sl_panContent.putConstraint(SpringLayout.NORTH, lblProject, 20, SpringLayout.SOUTH, lblStatus);
+        sl_panContent.putConstraint(SpringLayout.WEST, lblProject, 0, SpringLayout.WEST, lblName);
+        panContent.add(lblProject);
 
-	public boolean checkFields()
-	{
-		boolean isEmpty = false;
-		if(cmbName.getSelectedIndex() == 0 && cmbStatus.getSelectedIndex() == 0 &&
-		   cmbProject.getSelectedIndex() == 0)
-		{
-			isEmpty = true;
-		}
-		
-		return isEmpty;
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getSource() == getClose()) {
-			this.dispose();
-		} 
-		/****
-		 * DEV Insert Code statements here to filter the list of suppliers 
-		 * if at least one of the fields is not empty/ meaning may laman kahit isa sa fields
-		 * kung mern then proceed to filtering the list
-		 *****/
-		else if (checkFields() == false && e.getSource() == btnFilter) {
-			closed = false;
-			this.dispose();
-		}
-		else if (checkFields() == true && e.getSource() == btnFilter ){
-			/****
-			 * DEV Insert Code statements here to set the 
-			 * list to the original if not one of the fields is filled up/
-			 * meaning if All fields are empty wag mag filter but insetad revert it back to the original
-			 * list of suppliers
-			 *****/
-			closed = false;
-			this.dispose();
-		}
-		else if (e.getSource() == btnRemoveFilter){
-			/***
-			 * DEV insert code statements here to remove the filter and set the view table to the original
-			 * meaning yung walang filter...
-			*****/
-			closed = false;
-			this.dispose();
-		}
-	}
+        cmbProject = new JComboBox();
+        cmbProject.setEditable(true);
+        AutoCompleteDecorator.decorate(cmbProject);
+        sl_panContent.putConstraint(SpringLayout.WEST, cmbProject, 0, SpringLayout.WEST, cmbName);
+        sl_panContent.putConstraint(SpringLayout.SOUTH, cmbProject, 0, SpringLayout.SOUTH, lblProject);
+        sl_panContent.putConstraint(SpringLayout.EAST, cmbProject, 0, SpringLayout.EAST, cmbName);
+        cmbProject.setBackground(Color.WHITE);
+        panContent.add(cmbProject);
 
-	public void populateEmployeeNames() {
-		Iterator<Employee> iterator = employeeController.getAll();
-		ArrayList<String> employeeNames = new ArrayList();
-		employeeNames.add("");
-		while (iterator.hasNext()) {
-			employeeNames.add(iterator.next().getName());
-		}
-		cmbName.setModel(new DefaultComboBoxModel(employeeNames.toArray()));
-	}
-	
-	public void populateEmployeeStatus() {
-		Iterator<Employee> iterator = employeeController.getAll();
-		ArrayList<String> employeeStatus = new ArrayList();
-		employeeStatus.add("");
-		while (iterator.hasNext()) {
-			String status = iterator.next().getStatus();
-			employeeStatus.add(status);
-			
-		}
-		cmbStatus.setModel(new DefaultComboBoxModel(employeeStatus.toArray()));
-	}
-	
-	public void populateEmployeeProjects() {
-		ArrayList<String> employeeProjects = new ArrayList();
-		employeeProjects.add("");
-		Iterator<Project> projectsIterator = projectController.getAll();
-		while (projectsIterator.hasNext()) {
-			employeeProjects.add(projectsIterator.next().getName());
-			
-		}
-		cmbProject.setModel(new DefaultComboBoxModel(employeeProjects.toArray()));
-	}
+        getClose().addActionListener(this);
+
+        setContent(panMain);
+        populateEmployeeNames();
+        populateEmployeeStatus();
+        populateEmployeeProjects();
+        this.setVisible(true);
+        this.repaint();
+        this.revalidate();
+    }
+
+    public Iterator getValues() {
+
+        if (list != null) {
+            list.add((String) cmbName.getSelectedItem());
+            list.add((String) cmbStatus.getSelectedItem());
+            list.add((String) cmbProject.getSelectedItem());
+            return list.iterator();
+        }
+        return null;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public boolean checkFields() {
+        boolean isEmpty = false;
+        if (cmbName.getSelectedIndex() == 0 && cmbStatus.getSelectedIndex() == 0
+                && cmbProject.getSelectedIndex() == 0) {
+            isEmpty = true;
+        }
+
+        return isEmpty;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == getClose()) {
+            this.dispose();
+        } /**
+         * **
+         * DEV Insert Code statements here to filter the list of suppliers if at
+         * least one of the fields is not empty
+		 ****
+         */
+        else if (checkFields() == false && e.getSource() == btnFilter) {
+            closed = false;
+            this.dispose();
+        } else if (checkFields() == true && e.getSource() == btnFilter) {
+            /**
+             * **
+             * DEV Insert Code statements here to set the list to the original
+             * if not one of the fields is filled up/ All fields are empty
+			 ****
+             */
+            list = null;
+            closed = false;
+            this.dispose();
+        } else if (e.getSource() == btnRemoveFilter) {
+            /**
+             * *
+             * DEV insert code statements here to remove the filter and set the
+             * view table to the original meaning yung walang filter...
+			****
+             */
+            list = null;
+            closed = false;
+            this.dispose();
+        }
+    }
+
+    public void populateEmployeeNames() {
+        Iterator<Employee> iterator = employeeController.getAll();
+        ArrayList<String> employeeNames = new ArrayList();
+        employeeNames.add("");
+        while (iterator.hasNext()) {
+            employeeNames.add(iterator.next().getName());
+        }
+        cmbName.setModel(new DefaultComboBoxModel(employeeNames.toArray()));
+    }
+
+    public void populateEmployeeStatus() {
+        Iterator<Employee> iterator = employeeController.getAll();
+        ArrayList<String> employeeStatus = new ArrayList();
+        employeeStatus.add("");
+        while (iterator.hasNext()) {
+            String status = iterator.next().getStatus();
+            employeeStatus.add(status);
+
+        }
+        cmbStatus.setModel(new DefaultComboBoxModel(employeeStatus.toArray()));
+    }
+
+    public void populateEmployeeProjects() {
+        ArrayList<String> employeeProjects = new ArrayList();
+        employeeProjects.add("");
+        Iterator<Project> projectsIterator = projectController.getAll();
+        while (projectsIterator.hasNext()) {
+            employeeProjects.add(projectsIterator.next().getName());
+
+        }
+        cmbProject.setModel(new DefaultComboBoxModel(employeeProjects.toArray()));
+    }
 }
